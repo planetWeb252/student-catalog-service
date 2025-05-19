317453859e2d2264b52271264bdd3786143248b0
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-05-19 16:52:09 +0200

    initial commit

---
+++ src/main/java/com/student_catalog_service/student_catalog_service/StudentCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.student_catalog_service.student_catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student_catalog_service/student_catalog_service/StudentCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student_catalog_service.student_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
